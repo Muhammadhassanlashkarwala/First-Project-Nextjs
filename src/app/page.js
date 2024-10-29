33f10f24e69d0aa11db4bad980e3cd9f522efa1e
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-10-30 04:48:19 +0500

    push

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid bg-black  items-center justify-items-center min-h-screen  sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-5xl text-white">
-        Muhammad Hassan
+      <h1 className="text-6xl text-white">
+       Hello Next JS...
       </h1>
-      <h1 className="text-2xl text-white">
-       I'm frontend web developer!
+      <h1 className="text-3xl text-white">
+       I'm Muhammad Hassan!
       </h1>
     </div>
   );
